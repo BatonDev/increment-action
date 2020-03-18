f66c9ea09a0f9139efdeb63cacb902e408f7d9c3
Derek Overby <33785983+derekoverby-baton@users.noreply.github.com>
2020-03-18 08:36:37 -0500

    Update increment.js

--- increment.js
+++ increment.js
@@ -91,20 +91,21 @@ async function main() {
     const NEW_VERSION = NEW_VERSION_YEAR + '.' + NEW_VERSION_MONTH + '.' + NEW_VERSION_PATCH;
     console.log(`NEW_VERSION = ${NEW_VERSION}`);
 
-    // commit new version
-    console.log('committing new version');
-    await exec.exec('git', ['checkout', CURRENT_BRANCH]);
-    await exec.exec('mvn', ['build-helper:parse-version', 'versions:set', `-DnewVersion=${NEW_VERSION}`, 'versions:commit', '--no-transfer-progress']);
-    await exec.exec('git', ['add', 'pom.xml']);
-    await exec.exec('git', ['commit', '-m', 'GitHub Action: increment version']);
-
-    // tag new version
-    console.log('tagging new version');
+    // determine tag 
     await exec.exec('mvn', ['org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate', '-Dexpression=project.name', '-q', '-DforceStdout'], options);
     const PROJECT_NAME = myOutput; myOutput = '';
     await exec.exec('mvn', ['org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate', '-Dexpression=project.version', '-q', '-DforceStdout'], options);
     const PROJECT_VERSION = myOutput; myOutput = '';
     const TAG_NAME = PROJECT_NAME + '-' + PROJECT_VERSION;
+
+    // commit new version
+    console.log('committing new version');
+    await exec.exec('git', ['checkout', CURRENT_BRANCH]);
+    await exec.exec('mvn', ['build-helper:parse-version', 'versions:set', `-DnewVersion=${NEW_VERSION}`, 'versions:commit', '--no-transfer-progress']);
+    await exec.exec('git', ['add', 'pom.xml']);
+    await exec.exec('git', ['commit', '-m', TAG_NAME]);
+
+    // tag new version
     console.log(`tagging version ${TAG_NAME}`);
     await exec.exec('git', ['tag', '-a', TAG_NAME, '-m', TAG_NAME]);
     await exec.exec('git', ['push', '--follow-tags']);
